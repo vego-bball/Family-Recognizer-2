7d6632a40a5cb3103b3b8363cebb3ed10eddc05c
Veer <80690781+vego-bball@users.noreply.github.com>
2021-08-11 10:25:28 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+Webcam.set({
+    width: 350,
+    height: 270,
+    image_format: "jpeg",
+    jpeg_quality: 100
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src ="'+data_uri+'">';
+    });
+}
+
+console.log('ml5 version: ', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5B8iRsLqn/model.json', modelLoaded);
+
+function modelLoaded()
+{
+    console.log("Model Loaded! ");
+}
+
+function check()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error(error);        
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+        document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+    
+}
+
